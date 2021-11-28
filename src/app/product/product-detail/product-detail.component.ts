a49297d7d7e483437e96c99dadda0acfbc15c925
tetsunari <mtst.tty556@gmail.com>
2021-11-28 23:35:02 +0900

    model & ngfor, ngif

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Product } from '../../shared/models/product';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.css']
+  styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit {
+  product!: Product;
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.product = new Product(1, 'Angular入門書「天地創造の章」', 3800, '神は云った。「Angularあれ」。するとAngularが出来た。');
   }
 
 }
