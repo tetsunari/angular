094c3abdca6f8ee09155530420203428808a391d
tetsunari <mtst.tty556@gmail.com>
2021-11-30 00:17:32 +0900

    add service

--- src/app/product/product-detail/product-detail.component.ts
+++ src/app/product/product-detail/product-detail.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../shared/models/product';
+import { ProductService } from '../../shared/services/product.service';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
-  styleUrls: ['./product-detail.component.scss'],
+  styleUrls: ['./product-detail.component.scss']
 })
 export class ProductDetailComponent implements OnInit {
-  product!: Product;
+  product: Product;
 
-  constructor() { }
+  constructor(
+    private productService: ProductService,
+  ) {}
 
   ngOnInit() {
-    this.product = new Product(1, 'Angular入門書「天地創造の章」', 3800, '神は云った。「Angularあれ」。するとAngularが出来た。');
+    this.productService.get(2).subscribe((product: Product) => {
+      this.product = product;
+    });
   }
-
 }
