d5a871f73177f3e2faeb5b202b3eefbbe937532e
tetsunari <mtst.tty556@gmail.com>
2022-03-24 00:57:08 +0900

    setInterval, clearInterval

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './product/product-list/product-list.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ProductEditComponent } from './product/product-edit/product-edit.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
-    ProductDetailComponent
+    ProductDetailComponent,
+    ProductEditComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
